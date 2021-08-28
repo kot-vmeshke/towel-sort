a172abd6c27da60957cdb3824834de6534bb1cda
kot-vmeshke <kot-vmeshke.4@gmail.com>
2021-08-28 21:11:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let matrixCopy = matrix;
+  if (!matrixCopy) return [];
+  for (let i = 1; i < matrixCopy.length; i+=2) {
+      matrixCopy[i].reverse();
+  }
+  return matrixCopy.flat();
 }
